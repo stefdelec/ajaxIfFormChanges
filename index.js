01eaab14be220b86168478d44a3a2963f3cbb5f9
stefdelec <s.delecroix@gmail.com>
2017-02-10 17:43:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,8 @@
-/*
-This function should manage multiple forms and update it only if the form has changed.
-*/
-   
-
-$.fn.multiAjaxFormIfItHasChanged = function (arrayToBeProcess, callbackSuccess, callbackFailures) {
-
-        //arrayToBeProcess =[{ id:"",ajax:$.ajax()},{...}]
-        //init if it is first time
-       
-
-
-
-        $.fn.addNew = function(obj){
-            $(this).data("arrAjax").push(obj);
-            $("#" + obj.id).formChanges("listen")
-        }
-
-        //Save forms
-        $.fn.listenToArray = function() {
-            for(let item of arrayToBeProcess) $(this).addNew(item);
-        }
-
-        //On click
-        $(this).on("click", function () {
-           
-            let arrOfAjax = $(this).data("arrAjax").map(function (obj) {
-                //console.log(obj.ajax);
-                if ($("#" + obj.id).formChanges("check")) {                    
-                    return obj.ajax();
-                }
-                else return obj.ajax()
-            });
-        
-           
-            //$.when.apply(null, arrOfAjax).then(callbackSuccess, callbackFailures)
-        })
-
-        if ($(this).data("arrAjax") == undefined) {
-            $(this).data("arrAjax", []);
-            $(this).listenToArray(arrayToBeProcess);
-        }
-
-        return this;
-    }
 
 
 //FORM CHANGES
+// This jquery plugin "listen" and save a form, in order to check if there is differences between 2 states. 
+
     $.fn.formChanges = function (action) {
         // [{value:22,id:""},{..}..]
         $.fn._listen = function () {
@@ -86,4 +43,51 @@ $.fn.multiAjaxFormIfItHasChanged = function (arrayToBeProcess, callbackSuccess, 
         if (this == null) return false;
         else if (action == "listen") $(this).data("savedForm", $(this)._listen())
         else if (action == "check") return $(this)._check()
+    }
+
+    
+    /*
+This jquery plugin manage multiple forms and update it only if the form has changed.
+Forms are submitted on a click event only if their states actually changed.
+*/
+   
+
+$.fn.multiAjaxFormIfItHasChanged = function (arrayToBeProcess, callbackSuccess, callbackFailures) {
+
+        //arrayToBeProcess =[{ id:"",ajax:$.ajax()},{...}]
+        //init if it is first time
+       
+
+
+
+        $.fn.addNew = function(obj){
+            $(this).data("arrAjax").push(obj);
+            $("#" + obj.id).formChanges("listen")
+        }
+
+        //Save forms
+        $.fn.listenToArray = function() {
+            for(let item of arrayToBeProcess) $(this).addNew(item);
+        }
+
+        //On click
+        $(this).on("click", function () {
+           
+            let arrOfAjax = $(this).data("arrAjax").map(function (obj) {
+                //console.log(obj.ajax);
+                if ($("#" + obj.id).formChanges("check")) {                    
+                    return obj.ajax();
+                }
+                else return obj.ajax()
+            });
+   
+            $.when(...arrOfAjax).then(callbackSuccess, callbackFailures)
+        })
+
+        if ($(this).data("arrAjax") == undefined) {
+            $(this).data("arrAjax", []);
+            $(this).listenToArray(arrayToBeProcess);
+        }
+
+        return this;
     }
